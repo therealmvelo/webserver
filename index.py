73fb8603a4b66ce2e0d36808976810fa46605f91
therealmvelo <2020130474@students,elangeni.edu.za>
2025-03-09 21:30:50 +0200

    not working as expected but deployed anyway

--- index.py
+++ index.py
@@ -1,16 +1,18 @@
 import socket
 
 HOST = "127.0.0.1"
-PORT = 9000
+PORT = 8080
 
 RESPONSE = b"""\
-    HTTP /1.1 200 OK
-    Content-type: text/html 
-    Content-length: 15" 
+    HTTP/1.1 200 OK\r\n\
+    Content-type: text/html\r\n\
+    Content-length: 22\r\n\
+    Connection: close\r\n\
+    \r\n\
     
-    <h1>HELLO WORLD</h1>""".replace(b"\n",b"\r\n")
+    <h1>HELLO!</h1>""".replace(b"\n" , b"\r\n")
     
-with socket.socket() as server_sock:
+with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as server_sock:
     server_sock.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR, 1)
 
     server_sock.bind((HOST,PORT))
@@ -19,13 +21,10 @@ with socket.socket() as server_sock:
 
     print(f"listening on {HOST}:{PORT}")
 
-    
-
-    
-
     while True:
         client_sock,client_addr = server_sock.accept()
         print(f"new connection from {client_addr}")
-        
+
         with client_sock:
             client_sock.sendall(RESPONSE)
+            client_sock.shutdown(socket.SHUT_WR)
