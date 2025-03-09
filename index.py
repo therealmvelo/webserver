f1af3d9b606471f15a39fc84f9a90b24845b0819
therealmvelo <2020130474@students,elangeni.edu.za>
2025-03-09 20:04:41 +0200

    added RESPONSE from the seevr

--- index.py
+++ index.py
@@ -3,6 +3,13 @@ import socket
 HOST = "127.0.0.1"
 PORT = 9000
 
+RESPONSE = b"""\
+    HTTP /1.1 200 OK
+    Content-type: text/html 
+    Content-length: 15" 
+    
+    <h1>HELLO WORLD</h1>""".replace(b"\n",b"\r\n")
+    
 with socket.socket() as server_sock:
     server_sock.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR, 1)
 
@@ -15,3 +22,6 @@ with socket.socket() as server_sock:
     client_sock,client_addr = server_sock.accept()
 
     print(f"new connection from {client_addr}")
+
+    with client_sock:
+        client_sock.sendall(RESPONSE)
