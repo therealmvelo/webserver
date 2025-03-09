7f87cf2a1f30efda20ded5604f459f7a0c4acbd3
therealmvelo <2020130474@students,elangeni.edu.za>
2025-03-09 21:44:06 +0200

    fixed identation issues and removed regEx

--- index.py
+++ index.py
@@ -4,27 +4,23 @@ HOST = "127.0.0.1"
 PORT = 8080
 
 RESPONSE = b"""\
-    HTTP/1.1 200 OK\r\n\
-    Content-type: text/html\r\n\
-    Content-length: 22\r\n\
-    Connection: close\r\n\
-    \r\n\
-    
-    <h1>HELLO!</h1>""".replace(b"\n" , b"\r\n")
+HTTP/1.1 200 OK\r\n\
+Content-Type: text/html\r\n\
+Content-Length: 22\r\n\
+Connection: close\r\n\
+\r\n\
+<h1>HELLO WORLD</h1>"""
     
 with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as server_sock:
     server_sock.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR, 1)
-
     server_sock.bind((HOST,PORT))
-
-    server_sock.listen(0)
+    server_sock.listen(5)
 
     print(f"listening on {HOST}:{PORT}")
 
     while True:
         client_sock,client_addr = server_sock.accept()
         print(f"new connection from {client_addr}")
-
         with client_sock:
             client_sock.sendall(RESPONSE)
             client_sock.shutdown(socket.SHUT_WR)
