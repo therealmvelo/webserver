a2787c90cd451e89b95b8ad4652eb9eb4c51a904
therealmvelo <2020130474@students,elangeni.edu.za>
2025-03-09 19:05:15 +0200

    server

--- index.py
+++ index.py
@@ -1,2 +1,11 @@
 import socket
 
+HOST = "127.0.0.1"
+PORT = 9000
+
+with socket.socket() as server_sock:
+    server_sock.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR, 1)
+
+    server_sock.bind((HOST,PORT))
+
+    server_sock.listen(0)
